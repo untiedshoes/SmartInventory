3eb1f953d6f138f5c89a84d39bd6f88a2093438c
Craig Richards <geraldrichards@Craigs-MacBook-Pro.local>
2026-03-01 15:20:27 +0000

    Created the initial product controller

---
+++ src/SmartInventory.Web/ProductsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using SmartInventory.Core.Entities;
+using SmartInventory.Core.Interfaces;
+
+namespace SmartInventory.Web.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IProductService _service;
+
+    public ProductsController(IProductService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var products = await _service.GetAllAsync();
+        return Ok(products);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(Product product)
+    {
+        var created = await _service.CreateAsync(product);
+        return CreatedAtAction(nameof(GetAll), new { id = created.Id }, created);
+    }
+
+}
