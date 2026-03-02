6fd1ca9f2d5575749b1e45e9e17c3aff1c37b444
Craig Richards <geraldrichards@Craigs-MacBook-Pro.local>
2026-03-02 17:16:49 +0000

    fix: align FakeProductService namespace with project and update Program.cs import

--- src/SmartInventory.Services/FakeProductService.cs
+++ src/SmartInventory.Services/FakeProductService.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace SmartInventory.Services;
+namespace SmartInventory.Services
 {
     public class FakeProductService : IProductService
     {
